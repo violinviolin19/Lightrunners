20a41279f5ed18713907d6e843c952b6ac894f0a
Will Spencer <will.e.spencer@gmail.com>
2022-02-24 10:58:55 -0500

    Add Grunt cpp and h files based off player

---
+++ source/Grunt.cpp
@@ -0,0 +1,78 @@
+//
+//  Grunt.cpp
+//  Luminance
+//
+//  Created by Will Spencer on 2/24/22.
+//  Copyright © 2022 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#include "Grunt.h"
+
+#pragma mark Init
+/**
+ * Initializes a new grunt with the given position, size, and name.
+ *
+ * The grunt size is specified in world coordinates.
+ *
+ * The scene graph is completely decoupled from the physics system.
+ * The node does not have to be the same size as the physics body. We
+ * only guarantee that the scene graph node is positioned correctly
+ * according to the drawing scale.
+ *
+ * @param  pos      Initial position in world coordinates
+ * @param  size       The dimensions of the box.
+ * @param name The name of the grunt
+ *
+ * @return  true if the obstacle is initialized properly, false otherwise.
+ */
+bool Grunt::init(const Vec2 pos, const Size size, string name) {
+    CapsuleObstacle::init(pos, size);
+    setName(name);
+
+    _gruntNode = nullptr;
+    _health = 100;
+
+    setDensity(0.01f);
+    setFriction(0.0f);
+    setRestitution(0.01f);
+    setFixedRotation(true);
+
+    return true;
+}
+
+#pragma mark Animation & Drawing
+
+/**
+ * Sets the grunt scene graph node
+ *
+ * @param node the node to set it to
+ */
+void Grunt::setGruntNode(const std::shared_ptr<scene2::PolygonNode>& node) {
+    _gruntNode = node;
+}
+
+/**
+ * Update the scene graph.
+ *
+ * @param delta the timing value
+ */
+void Grunt::update(float delta) {
+    CapsuleObstacle::update(delta);
+    if (_gruntNode != nullptr) {
+        _gruntNode->setPosition(getPosition());
+    }
+}
+
+#pragma mark Movement
+/**
+ * Moves the grunt by the specified amount.
+ *
+ * @param forwardX Amount to move in the x direction
+ * @param forwardY Amount to move in the y direction
+ */
+void Grunt::move(float forwardX, float forwardY) {
+    setVX(1000 * forwardX);
+    setVY(1000 * forwardY);
+    if (forwardX == 0) setVX(0);
+    if (forwardY == 0) setVY(0);
+}
---
+++ source/Grunt.h
@@ -0,0 +1,111 @@
+#pragma once
+//
+//  Grunt.h
+//  Luminance
+//
+//  Created by Will Spencer on 2/24/22.
+//  Copyright © 2022 Game Design Initiative at Cornell. All rights reserved.
+//
+
+#ifndef Grunt_h
+#define Grunt_h
+
+#include <stdio.h>
+#include <cugl/cugl.h>
+
+using namespace cugl;
+using namespace physics2;
+
+class Grunt : public CapsuleObstacle {
+private:
+
+    /** Grunt health */
+    int _health;
+
+    /** The scene graph node for the grunt */
+    std::shared_ptr<cugl::scene2::SceneNode> _gruntNode;
+
+    /** Force to be applied to the grunt */
+    Vec2 _force;
+
+public:
+
+#pragma mark Constructors
+    /**
+     * Creates a grunt with the given position and data
+     *
+     * @param pos The grunt position
+     * @param data The data defining the grunt
+     */
+    Grunt(void) : CapsuleObstacle() { }
+
+    /**
+     * Disposes the grunt
+     */
+    ~Grunt() {}
+
+    /**
+     * Initializes a new grunt with the given position and size.
+     *
+     * @param  pos      Initial position in world coordinates
+     * @param  size       The dimensions of the box.
+     *
+     * @return  true if the obstacle is initialized properly, false otherwise.
+     */
+    virtual bool init(const Vec2 pos, const Size size, string name);
+
+#pragma mark Static Constructors
+    /**
+     * Returns a new capsule object at the given point with no size.
+     *
+     * @param pos   Initial position in world coordinates
+     *
+     * @return a new capsule object at the given point with no size.
+     */
+    static std::shared_ptr<Grunt> alloc(const Vec2 pos, const cugl::Size& size, string name) {
+        std::shared_ptr<Grunt> result = std::make_shared<Grunt>();
+        return (result->init(pos, size, name) ? result : nullptr);
+    }
+
+#pragma mark Properties
+
+    /**
+     * Returns the current health of the grunt.
+     *
+     * @return the current health.
+     */
+    int getHealth() const { return _health; }
+
+    /**
+     * Sets the current grunt's health.
+     *
+     * @param value The current grunt health.
+     */
+    void setHealth(int value) { _health = value; }
+
+    /**
+     * Update the scene graph.
+     *
+     * @param delta the timing value
+     */
+    void update(float delta);
+
+#pragma mark Graphics
+
+    /**
+     * Sets the scene graph node representing this grunt.
+     *
+     * @param node  The scene graph node representing this grunt.
+     */
+    void setGruntNode(const std::shared_ptr<cugl::scene2::PolygonNode>& node);
+
+#pragma mark Movement
+    /**
+     * Moves the grunt by the specified amount.
+     *
+     * @param forwardX Amount to move in the x direction
+     * @param forwardY Amount to move in the y direction
+     */
+    void move(float forwardX, float forwardY);
+};
+#endif /* Player_hpp */
