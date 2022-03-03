330c2454e01dff8fde833db58a729c95f6650da6
Will Spencer <will.e.spencer@gmail.com>
2022-03-03 16:01:44 -0500

    Define AIController methods

--- source/controllers/AIController.cpp
+++ source/controllers/AIController.cpp
@@ -1,3 +1,23 @@
 #include "AIController.h"
 
 #include <cugl/cugl.h>
+
+AIController::AIController(){};
+
+void moveEnemyTowardLocation(std::shared_ptr<Grunt> enemy, cugl::Vec2 p) {
+  cugl::Vec2 diff = p - enemy->getPosition();
+
+  // TODO turn this into some sort of variable somewhere
+  float step = 0.2f;
+  diff.scale(step);
+  enemy->move(diff.x, diff.y);
+}
+
+void moveEnemiesTowardPlayer(EnemySet enemySet,
+                             std::shared_ptr<Player> player) {
+  std::unordered_set<std::shared_ptr<Grunt>> enemies = enemySet.getEnemies();
+  auto it = enemies.begin();
+  while (it != enemies.end()) {
+    moveEnemyTowardLocation((*it), player->getPosition());
+  }
+}
--- source/controllers/AIController.h
+++ source/controllers/AIController.h
@@ -2,9 +2,41 @@
 #define CONTROLLERS_AI_CONTROLLER_H_
 #include <cugl/cugl.h>
 
+#include "../models/EnemySet.h"
+#include "../models/Player.h"
+
 /**
  * A class to handle enemy AI.
  */
-class AIController {};
+class AIController {
+ private:
+  /*
+   * Moves the singular enemy toward the location specified
+   *
+   * @param enemy set of enemies to move
+   * @param p position to move enemy towards
+   */
+  void moveEnemyTowardLocation(Grunt enemy, cugl::Vec2 p);
+
+ public:
+  /**
+   * Creates a new AI controller with the default settings.
+   */
+  AIController();
+
+  /**
+   * Disposses this input controller, releasing all resources.
+   */
+  ~AIController() {}
+
+  /*
+   * Moves all the enemies in the enemy set towards the player character.
+   *
+   * @param enemies set of enemies to move
+   * @param player player for enemies to move towards
+   */
+  void moveEnemiesTowardPlayer(EnemySet enemies,
+                               std::shared_ptr<Player> player);
+};
 
 #endif /* CONTROLLERS_AI_CONTROLLER_H_ */
