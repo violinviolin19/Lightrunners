bf1d2d800047bc310a84625f7f2c6f856da32db4
Will Spencer <will.e.spencer@gmail.com>
2022-03-03 15:08:33 -0500

    Add empty AI Controller files, update windows project

---
+++ source/controllers/AIController.cpp
@@ -0,0 +1,3 @@
+#include "AIController.h"
+
+#include <cugl/cugl.h>
---
+++ source/controllers/AIController.h
@@ -0,0 +1,10 @@
+#ifndef CONTROLLERS_AI_CONTROLLER_H_
+#define CONTROLLERS_AI_CONTROLLER_H_
+#include <cugl/cugl.h>
+
+/**
+ * A class to handle enemy AI.
+ */
+class AIController {};
+
+#endif /* CONTROLLERS_AI_CONTROLLER_H_ */
